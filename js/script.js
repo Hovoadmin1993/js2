f7e82d6fbcce009492c93cf8096933bdac7ea673
Hovhannes Petrosyan <Hovopetrosyan7@gmail.com>
2021-05-17 12:31:23 +0400

    js

---
+++ js/script.js
@@ -0,0 +1,91 @@
+// // Ex N12
+
+// let n = Math.abs( +prompt("drakan tiv",""));
+// let s = 0;
+// let i = 1;
+
+// for (i = 1; i <= n; i++) {
+//     s += Math.pow(i, 2);
+    
+// }
+
+// console.log(s);
+
+
+// ____________________________________________________________
+
+
+// // Ex N13
+
+// let i = +prompt("num i", "");
+// let k = 1;
+// let y = 1;
+
+// do{
+//   y *= 2;
+//   k++;
+
+// }while(k <= i);
+
+// console.log(y);
+
+
+// _____________________________________________________
+
+
+// // Ex N14
+
+
+// let a = Math.round(+prompt("drakan tiv",""));
+// let s = 0;
+// let i = 1;
+
+// if(a <= 0){
+//     a = 1;
+// }
+
+// for (i = 1; i <= a; i++) {
+//     s += Math.pow(i, 2);
+// }
+
+// console.log(s);
+
+
+// ___________________________________________________
+
+
+// Ex N15
+
+// let a = Math.round(+prompt("drakan tiv",""));
+// let s = 0;
+// let i = 1;
+
+// if(a <= 0){
+//     a = 1;
+// }
+
+// for (i = 1; i <= a; i++) {
+//     s += Math.pow(2, i);
+// }
+
+// console.log(s);
+
+
+
+// ________________________________________________
+
+
+
+// Ex N16
+
+// let n = Math.abs( +prompt("drakan tiv",""));
+// let s = 0;
+// let h = 0;
+// let i = 1;
+
+// for (i = 1; i <= n; i++) {
+//     h += Math.sin(i);
+//     s += i / h;
+// }
+
+// console.log(s);
